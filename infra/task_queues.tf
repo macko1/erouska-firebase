001962549a83db6b9eed10acedabb8a149c56642
Jakub Beránek <berykubik@gmail.com>
2020-04-07 14:38:45 +0200

    Add task queues

---
+++ infra/task_queues.tf
@@ -0,0 +1,11 @@
+resource "google_cloud_tasks_queue" "upload-removal" {
+  name     = "upload-removal"
+  location = var.region
+  rate_limits {
+    max_concurrent_dispatches = 100
+  }
+  retry_config {
+    max_attempts = 3
+    min_backoff  = "5s"
+  }
+}
